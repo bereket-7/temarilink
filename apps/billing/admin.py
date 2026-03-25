ec00d7d7b0ead6c73ce454e1878143ae662c5715
Bereket Honelign <bekhonelign@gmail.com>
2026-03-25 09:43:50 +0300

    admin , apps,

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Fee, Payment
 
-# Register your models here.
+@admin.register(Fee)
+class FeeAdmin(admin.ModelAdmin):
+    list_display = ('student', 'amount', 'status', 'due_date')
+    list_filter = ('status', 'due_date')
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('fee', 'amount', 'paid_at')
+    readonly_fields = ('paid_at',)
---
+++ apps/billing/models/__init__.py
@@ -0,0 +1,2 @@
+from .fee import Fee
+from .payment import Payment
