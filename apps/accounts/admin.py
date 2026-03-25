3b4706487e510f6b08a0479aab3a5466bb0a787a
Bereket Honelign <bekhonelign@gmail.com>
2026-03-25 09:43:15 +0300

    accounts admin and models

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'role', 'school', 'is_active')
+    list_filter = ('role', 'school', 'is_active')
+    search_fields = ('full_name', 'phone')
